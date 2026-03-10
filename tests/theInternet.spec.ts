df5c2100eb80670052809a174a8ed00beec75cfb
johnChow-tech <shulking@hotmail.com>
2026-03-10 16:33:16 +0900

    test(e2e): 增加测试用例 * A/B Testing * Add/Remove Elements

---
+++ tests/theInternet.spec.ts
@@ -0,0 +1,32 @@
+import test, { expect } from "@playwright/test";
+
+const DOMAIN = 'https://the-internet.herokuapp.com'
+
+test('A/B Testing', async ({ page }) => {
+    await page.goto(`${DOMAIN}/abtest`)
+    const heading = page.getByRole('heading');
+    await expect(heading).toHaveCount(1)
+    const reg = new RegExp('A/B')
+    await expect(heading).toHaveText(reg)
+});
+
+test('Add/Remove Elements', async ({ page }) => {
+    await page.goto(`${DOMAIN}/add_remove_elements/`);
+    await expect(page.getByRole('heading', { name: 'Add/Remove Elements' })).toBeVisible()
+
+    const addBtn = page.getByRole('button', { name: 'Add Element' });
+    await expect(addBtn).toHaveCount(1)
+    const deleteBtn = page.getByRole('button', { name: 'Delete' })
+    await expect(deleteBtn).toHaveCount(0)
+
+    const addTimes = 10
+    for (let i = 1; i <= addTimes; i++) {
+        await addBtn.click()
+    }
+    await expect(deleteBtn).toHaveCount(addTimes)
+
+    for (let i = addTimes - 1; i >= 0; i--) {
+        await deleteBtn.first().click()
+    }
+    await expect(deleteBtn).toHaveCount(0)
+})
